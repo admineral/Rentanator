f3ee977129c48359dc4dead0e7efb64c2e19225c
Elias <50579369+admineral@users.noreply.github.com>
2024-03-04 18:18:20 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function AgentsPage() {
   const InfoCard = (
     <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
       <h1 className="text-3xl md:text-4xl mb-4">
-        🏠 RentPaca: Extracting Key Data from Rental Agreements 📑🔍
+        🏠Extracting Key Data from Rental Agreements 📑🔍
       </h1>
       <ul>
         <li className="text-l">
